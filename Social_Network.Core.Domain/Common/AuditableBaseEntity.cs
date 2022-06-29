e0e9b7ba46147dbbad6746ce0b2d9117d21c39ca
Javier Perez <javierpdc13@hotmail.com>
2022-06-28 20:28:38 -0400

    Add common properties to entities

---
+++ Social_Network.Core.Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_Network.Core.Domain.Common
+{
+    public class AuditableProperties
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
