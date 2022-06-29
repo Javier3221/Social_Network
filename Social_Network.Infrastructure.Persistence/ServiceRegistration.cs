9a0be782a232d8b37c7ab87d5931f78949143ef0
Javier Perez <javierpdc13@hotmail.com>
2022-06-28 20:30:19 -0400

    Add dependency injection to context

---
+++ Social_Network.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Social_Network.Infrastructure.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_Network.Infrastructure.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Context
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+
+            #endregion
+        }
+    }
+}
