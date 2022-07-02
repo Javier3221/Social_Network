de856e0f49838ad0999cceef1a3d90f416df6f45
Javier Perez <javierpdc13@hotmail.com>
2022-07-02 16:47:22 -0400

    Configure automapper mappings

---
+++ Social_Network.Core.Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Social_Network.Core.Application.ViewModels.Commentary;
+using Social_Network.Core.Application.ViewModels.Friend;
+using Social_Network.Core.Application.ViewModels.Post;
+using Social_Network.Core.Application.ViewModels.User;
+using Social_Network.Core.Domain.Entities;
+
+namespace Social_Network.Core.Application.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<SaveUserViewModel, User>()
+                .ForMember(dest => dest.Posts, opt => opt.Ignore())
+                .ForMember(dest => dest.Friends, opt => opt.Ignore())
+                .ForMember(dest => dest.FriendWith, opt => opt.Ignore())
+                .ReverseMap()
+                .ForMember(dest => dest.ConfirmPassword, opt => opt.Ignore())
+                .ForMember(dest => dest.ProfileImage, opt => opt.Ignore());
+
+            CreateMap<PostViewModel, Post>()
+                .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+                .ForMember(dest => dest.DateCreated, opt => opt.Ignore());
+
+            CreateMap<Post, SavePostViewModel>()
+                .ForMember(dest => dest.PostImage, opt => opt.Ignore())
+                .ReverseMap()
+                .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+                .ForMember(dest => dest.Commentaries, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.DateCreated, opt => opt.Ignore());
+
+            CreateMap<FriendViewModel, Friend>()
+                .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+                .ForMember(dest => dest.DateCreated, opt => opt.Ignore());
+
+            CreateMap<CommentaryViewModel, Commentary>()
+                .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+                .ForMember(dest => dest.DateCreated, opt => opt.Ignore());
+        }
+    }
+}
