c8d98124273e1e9ec4cc826489a2e3a4dd227296
Javier Perez <javierpdc13@hotmail.com>
2022-07-02 16:51:12 -0400

    Create ValidateUserSession.cs

---
+++ Social_Network/Models/Middlewares/ValidateUserSession.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Social_Network.Core.Application.Helpers;
+using Social_Network.Core.Application.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Social_Network.Models.Middlewares
+{
+    public class ValidateUserSession
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ValidateUserSession(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public bool HasUser()
+        {
+            UserViewModel userViewModel = _httpContextAccessor.HttpContext.Session.Get<UserViewModel>("user");
+
+            return userViewModel != null;
+        }
+    }
+}
