674fc6be813e8b5aaadbaacceceda327eeb16ee8
Javier Perez <javierpdc13@hotmail.com>
2022-07-02 19:46:03 -0400

    Create UploadImages.cs

---
+++ Social_Network.Core.Application/Helpers/UploadImages.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_Network.Core.Application.Helpers
+{
+    public static class UploadImages
+    {
+        public static string UploadFile(List<IFormFile> files, int id, bool isEditMode = false, string imageUrls = "")
+        {
+            if (isEditMode && files == null)
+            {
+                string paths = string.Join(",", imageUrls);
+
+                return paths;
+            }
+            string basePath = $"/images/Profiles/{id}";
+            string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot{basePath}");
+
+            //create folder if it doesn't exists
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            List<string> pathList = new();
+            foreach (IFormFile file in files)
+            {
+                Guid guid = Guid.NewGuid();
+                FileInfo fileInfo = new(file.FileName);
+                string filename = guid + fileInfo.Extension;
+
+                string fileNameWithPath = Path.Combine(path, filename);
+
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                pathList.Add($"{basePath}/{filename}");
+            }
+
+            string pathString = string.Join(",", pathList);
+
+            if (isEditMode)
+            {
+                List<string> oldPath = imageUrls.Split(',').ToList();
+
+                foreach (string item in oldPath)
+                {
+                    string[] oldImagePart = item.Split("/");
+                    string oldImageName = oldImagePart[^1];
+                    string completeImageOldPath = Path.Combine(path, oldImageName);
+
+                    if (System.IO.File.Exists(completeImageOldPath))
+                    {
+                        System.IO.File.Delete(completeImageOldPath);
+                    }
+                }
+            }
+
+            return pathString;
+        }
+    }
+}
