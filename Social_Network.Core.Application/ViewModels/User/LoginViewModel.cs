1ce220f9e196e4a21d2a22923b57df792d3025b4
Javier Perez <javierpdc13@hotmail.com>
2022-07-02 16:49:50 -0400

    Create LoginViewModel.cs

---
+++ Social_Network.Core.Application/ViewModels/User/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_Network.Core.Application.ViewModels.User
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "You need to specify your UserName")]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "You need to specify your password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
