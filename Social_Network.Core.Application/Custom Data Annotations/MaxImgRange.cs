7f0e6cd2d93bfce2237fee6075fed4377028cb1e
Javier Perez <javierpdc13@hotmail.com>
2022-07-02 16:28:52 -0400

    Create MaxImgRange.cs

---
+++ Social_Network.Core.Application/Custom Data Annotations/MaxImgRange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_Network.Core.Application.Custom_Data_Annotations
+{
+    public class MaxImgRange : ValidationAttribute
+    {
+        private readonly int _maxElements;
+
+        public MaxImgRange(int maxElements)
+        {
+            _maxElements = maxElements;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var list = value as IList;
+            if (list == null)
+            {
+                return true;
+            }
+            else if (list != null)
+            {
+                return list.Count <= _maxElements && list.Count > 0;
+            }
+            return false;
+        }
+    }
+}
