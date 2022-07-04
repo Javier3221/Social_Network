89f7b9585c24f10ab8bf80358e57421698abacd9
Javier Perez <javierpdc13@hotmail.com>
2022-07-03 21:49:09 -0400

    Create PostController.cs

---
+++ Social_Network/Controllers/PostController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Social_Network.Core.Application.Helpers;
+using Social_Network.Core.Application.Interfaces.Services;
+using Social_Network.Core.Application.ViewModels.Post;
+using Social_Network.Models.Middlewares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Social_Network.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService _postService;
+        private readonly ValidateUserSession _validateUserSession;
+
+        public PostController(IPostService postService, ValidateUserSession validateUserSession)
+        {
+            _postService = postService;
+            _validateUserSession = validateUserSession;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string description, IFormFile postImage = null)
+        {
+            if (!_validateUserSession.HasUser())
+            {
+                return RedirectToRoute(new { controller="User", action="Login"});
+            }
+
+            SavePostViewModel savePostVm = new();
+            savePostVm.PostDescription = description;
+            savePostVm.PostImage = postImage;
+
+            if (ModelState.IsValid)
+            {
+               savePostVm = await _postService.Add(savePostVm);
+                if (savePostVm != null && savePostVm.Id != 0 && postImage != null)
+                {
+                    savePostVm.ImgUrl = UploadImages.UploadFile(new List<IFormFile> { postImage }, savePostVm.Id, "Posts");
+                    await _postService.Update(savePostVm, savePostVm.Id);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("descriptionValidation", "You need to write something before posting");
+            }
+
+            return RedirectToRoute(new { controller="Home", action="Index"});
+        }
+    }
+}
