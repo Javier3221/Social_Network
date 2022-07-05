acc6867e73d9bad634b77c885f721b25ecc58f4f
Javier Perez <javierpdc13@hotmail.com>
2022-07-04 21:44:25 -0400

    Add override to add method

--- Social_Network.Core.Application/Services/CommentaryService.cs
+++ Social_Network.Core.Application/Services/CommentaryService.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Social_Network.Core.Application.Helpers;
 using Social_Network.Core.Application.Interfaces.Repositories;
 using Social_Network.Core.Application.Interfaces.Services;
 using Social_Network.Core.Application.ViewModels.Commentary;
+using Social_Network.Core.Application.ViewModels.User;
 using Social_Network.Core.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -15,11 +18,20 @@ namespace Social_Network.Core.Application.Services
     {
         private readonly ICommentaryRepository _repository;
         private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public CommentaryService(ICommentaryRepository repo, IMapper mapper) : base(repo, mapper)
+        public CommentaryService(ICommentaryRepository repo, IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(repo, mapper)
         {
             _repository = repo;
             _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public override async Task<CommentaryViewModel> Add(CommentaryViewModel postVm)
+        {
+            postVm.UserId = _httpContextAccessor.HttpContext.Session.Get<UserViewModel>("user").Id;
+
+            return await base.Add(postVm);
         }
     }
 }
