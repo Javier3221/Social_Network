7a490bf199f2ca0df0f6b9de9e0872fe04d6addb
Javier Perez <javierpdc13@hotmail.com>
2022-07-06 16:31:54 -0400

    Update FriendViewModel.cs

--- Social_Network.Core.Application/ViewModels/Friend/FriendViewModel.cs
+++ Social_Network.Core.Application/ViewModels/Friend/FriendViewModel.cs
@@ -9,8 +9,10 @@ namespace Social_Network.Core.Application.ViewModels.Friend
 {
     public class FriendViewModel
     {
-        public int FriendFrom { get; set; }
-        public int FriendTo { get; set; }
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FriendsWith { get; set; }
+        public DateTime DateCreated { get; set; }
 
         public UserViewModel User { get; set; }
         public UserViewModel UserFriend { get; set; }
